b570f1a607f478c281bea7ae98e48dcac9a01658
ifengc <timlmj@gmail.com>
2016-08-13 07:25:07 +0000

    add w2v gotcha function

--- strmatch-experiment/strmatch.py
+++ strmatch-experiment/strmatch.py
@@ -26,7 +26,7 @@ def is_in_lines(sent):
 def strmatch(sents):
 
     sents_is_matched = [is_in_lines(sent) for sent in sents]
-    print(sents_is_matched)
+#    print(sents_is_matched)
     return [choice
             for is_matched, choice in zip(sents_is_matched, ascii_lowercase)
             if is_matched]
@@ -36,22 +36,36 @@ import subprocess
 def grepsents(sents):
     return first(choice for sent, choice in zip(sents, ascii_lowercase)
                  if subprocess.run(['grep', '-Fl', sent, '/pix/zh-all.txt'
-                                   ]).returncode == 0)
+                                   ], stdout=subprocess.PIPE).returncode == 0)
 
 
-def grep_question(question):
+def grep_question(question, choiceList):
     pre_str, post_str, *_ = question.split('︽⊙＿⊙︽')
     cmd = "grep -F '{pre_str}' /pix/zh-all.txt | grep -Po '(?<={pre_str}).+(?={post_str})'".format(pre_str=pre_str, post_str=post_str)
     matched_choice = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-    return matched_choice
+    return gotcha(matched_choice, choiceList)
 
+import gensim
+
+def gotcha(keyword, choiceList):
+    if len(keyword) == 0:
+        return -1
+    else:
+        choice = ['a','b','c','d','e']
+        for i in range(len(choiceList)):
+            try:
+                choiceList[i] = model.similarity(keyword, choiceList[i])
+            except KeyError:
+                choiceList[i] = -999999999
+        return choice[choiceList.index(max(choiceList))]
 
 if __name__ == '__main__':
     test_question = '一年沒再踏入︽⊙＿⊙︽，這裡變化真的相當多，不知道下次再踏入這塊土地，是否又會有不一樣的感覺呢'
-    test_answer_choices = '涼拌菜', '邊框', '生田', '金金', '墾丁'
+    test_answer_choices = ['飛機', '邊框', '田', '金金', '墾丁']
+    model = gensim.models.Word2Vec.load_word2vec_format('/home/ifengc/dav/word2vec/data/zhseg-all-vector.bin', binary=True)
     start_time = time.time()
     sents = gen_answer_sentences(test_question, test_answer_choices)
     print(grepsents(sents))
-    print(grep_question(test_question))
+    print(grep_question(test_question, test_answer_choices))
     elapsed_time = time.time() - start_time
-    print('find elapsed time', elapsed_time)
+#    print('find elapsed time', elapsed_time)
